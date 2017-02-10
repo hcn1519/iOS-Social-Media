c34f712062daf86e0cdecb840043d5f9dd5ea83c
Hong, Chang Nam <hcn1519@gmail.com>
2017-02-10 12:55:40 +0900

    textDeco

---
+++ SocialMedia/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  SocialMedia
+//
+//  Created by 홍창남 on 2017. 2. 10..
+//  Copyright © 2017년 홍창남. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
